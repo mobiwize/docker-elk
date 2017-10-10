a24324b6e854d0e76ed385733ad5ea7fbf882826
Slava Shpitalny <slavik57@gmail.com>
2017-10-10 08:48:35 +0300

    Set the default number of replicas for the new indices

--- elasticsearch/set_default_shards_and_replicas.sh
+++ elasticsearch/set_default_shards_and_replicas.sh
@@ -5,7 +5,12 @@ if [[ -z "${DEFAULT_NUMBER_OF_SHARDS// }" ]]; then
   exit 1
 fi
 
-echo "\nSetting the default number of shards to be $DEFAULT_NUMBER_OF_SHARDS"
+if [[ -z "${DEFAULT_NUMBER_OF_REPLICAS// }" ]]; then
+  echo "ERROR: DEFAULT_NUMBER_OF_REPLICAS is not defined"
+  exit 1
+fi
+
+echo "Setting the default number of shards to be $DEFAULT_NUMBER_OF_SHARDS and default number of replicas to be $DEFAULT_NUMBER_OF_REPLICAS"
 
 res="{}"
 while [ $res == "{}" ]; do
@@ -21,8 +26,15 @@ sed -e '2d;3d;$d' template.old.json > template.new.json
 if ! grep -q number_of_shards template.new.json; then
   sed -i "s/\"index\" \: {/\"index\" \: {\n\"number_of_shards\" \: \"$DEFAULT_NUMBER_OF_SHARDS\",/g" template.new.json
 else
-  sed -iE "s/\"number_of_shards\" \: \"[0-9][0-9]*\"/\"number_of_shards\" \: \"$DEFAULT_NUMBER_OF_SHARDS\"/g" template.new.json
+  sed -i "s/\"number_of_shards\" \: \"[0-9][0-9]*\"/\"number_of_shards\" \: \"$DEFAULT_NUMBER_OF_SHARDS\"/g" template.new.json
 fi
+
+if ! grep -q number_of_replicas template.new.json; then
+  sed -i "s/\"index\" \: {/\"index\" \: {\n\"number_of_replicas\" \: \"$DEFAULT_NUMBER_OF_REPLICAS\",/g" template.new.json
+else
+  sed -i "s/\"number_of_replicas\" \: \"[0-9][0-9]*\"/\"number_of_replicas\" \: \"$DEFAULT_NUMBER_OF_REPLICAS\"/g" template.new.json
+fi
+
 
 curl -XPUT 'http://localhost:9200/_template/logstash' -d @template.new.json --header "Content-Type: application/json"
 
